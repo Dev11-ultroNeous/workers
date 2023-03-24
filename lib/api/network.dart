999da95a745d9dd2c4bd3f2593d24b4180744ee5
Maulik <dev11.ultroneous@gmail.com>
2023-03-24 19:23:25 +0530

    isolate id print

--- lib/api/network.dart
+++ lib/api/network.dart
@@ -1,4 +1,6 @@
 import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
 import 'dart:isolate';
 
 import 'package:http/http.dart' as http;
@@ -7,10 +9,13 @@ import 'package:workers/sql/database_helper.dart';
 
 class NetworkApi {
   static final DatabaseHelper databaseHelper = injection<DatabaseHelper>();
+
   @pragma('vm:entry-point')
   static void apiOne(SendPort sendPort) async {
     init();
     print("Call API 1");
+    print("Isolate Id 1 ${Service.getIsolateID(Isolate.current)}");
+    print("Process Id 1 $pid");
     final data = await http
         .get(Uri.parse('https://random-data-api.com/api/v2/users?size=20'));
     final dataList = jsonDecode(data.body);
@@ -24,6 +29,8 @@ class NetworkApi {
   static void apiTwo(SendPort sendPort) async {
     init();
     print("Call API 2");
+    print("Isolate Id 2 ${Service.getIsolateID(Isolate.current)}");
+    print("Process Id 2 $pid");
     final data = await http
         .get(Uri.parse('https://random-data-api.com/api/v2/banks?size=20'));
     final dataList = jsonDecode(data.body);
@@ -37,6 +44,8 @@ class NetworkApi {
   static void apiThree(SendPort sendPort) async {
     init();
     print("Call API 3");
+    print("Isolate Id 3 ${Service.getIsolateID(Isolate.current)}");
+    print("Process Id 3 $pid");
     final data = await http
         .get(Uri.parse('https://random-data-api.com/api/v2/users?size=20'));
     final dataList = jsonDecode(data.body);
@@ -50,6 +59,8 @@ class NetworkApi {
   static void apiFour(SendPort sendPort) async {
     init();
     print("Call API 4");
+    print("Isolate Id 4 ${Service.getIsolateID(Isolate.current)}");
+    print("Process Id 4 $pid");
     final data = await http
         .get(Uri.parse('https://random-data-api.com/api/v2/users?size=20'));
     final dataList = jsonDecode(data.body);
@@ -63,6 +74,8 @@ class NetworkApi {
   static void apiFive(SendPort sendPort) async {
     init();
     print("Call API 5");
+    print("Isolate Id 5 ${Service.getIsolateID(Isolate.current)}");
+    print("Process Id 5 $pid");
     final data = await http
         .get(Uri.parse('https://random-data-api.com/api/v2/banks?size=20'));
     final dataList = jsonDecode(data.body);
@@ -76,6 +89,8 @@ class NetworkApi {
   static void apiSix(SendPort sendPort) async {
     init();
     print("Call API 6");
+    print("Isolate Id 6 ${Service.getIsolateID(Isolate.current)}");
+    print("Process Id 6 $pid");
     final data = await http
         .get(Uri.parse('https://random-data-api.com/api/v2/users?size=20'));
     final dataList = jsonDecode(data.body);
@@ -89,6 +104,8 @@ class NetworkApi {
   static void apiSeven(SendPort sendPort) async {
     init();
     print("Call API 7");
+    print("Isolate Id 7 ${Service.getIsolateID(Isolate.current)}");
+    print("Process Id 7 $pid");
     final data = await http.get(Uri.parse(
         'https://newsapi.org/v2/top-headlines?country=in&apiKey=40a99afefc5c4312a7fd02a620f5848a&pageSize=20'));
     final dataList = jsonDecode(data.body);
@@ -102,6 +119,8 @@ class NetworkApi {
   static void apiEight(SendPort sendPort) async {
     init();
     print("Call API 8");
+    print("Isolate Id 8 ${Service.getIsolateID(Isolate.current)}");
+    print("Process Id 8 $pid");
     final data = await http
         .get(Uri.parse('https://random-data-api.com/api/v2/users?size=20'));
     final dataList = jsonDecode(data.body);
@@ -115,6 +134,8 @@ class NetworkApi {
   static void apiNine(SendPort sendPort) async {
     init();
     print("Call API 9");
+    print("Isolate Id 9 ${Service.getIsolateID(Isolate.current)}");
+    print("Process Id 9 $pid");
     final data = await http
         .get(Uri.parse('https://random-data-api.com/api/v2/banks?size=20'));
     final dataList = jsonDecode(data.body);
@@ -128,6 +149,8 @@ class NetworkApi {
   static apiTen(SendPort sendPort) async {
     init();
     print("Call API 10");
+    print("Isolate Id 10 ${Service.getIsolateID(Isolate.current)}");
+    print("Process Id 10 $pid");
     final data = await http
         .get(Uri.parse('https://random-data-api.com/api/v2/users?size=20'));
     final dataList = jsonDecode(data.body);
